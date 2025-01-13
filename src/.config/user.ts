52c8aeff9aaeacc91abd2c9362d111081a2b53d6
Akirako <w.akirako@gmail.com>
2025-01-13 10:03:45 +0800

    创建我的 BLOG

--- src/.config/user.ts
+++ src/.config/user.ts
@@ -2,6 +2,29 @@ import type { UserConfig } from '~/types'
 
 export const userConfig: Partial<UserConfig> = {
   // Override the default config here
-  // site: { title: "講評世界" },
-  // seo: { twitter: "@moeyua13" },
+  site: {
+    title: '英子小屋',
+    subtitle: 'Akirako',
+    author: 'Akirako',
+    description: '用文字描述生活中、工作中和学习中想要保存住的瞬间，BLOG记录生活',
+    socialLinks: [
+      {
+        name: 'github',
+        href: 'https://github.com/Akirako',
+      },
+      {
+        name: 'youtube',
+        href: 'https://www.youtube.com/@akirakow.9918',
+      },
+      {
+        name: 'twitter',
+        href: 'https://x.com/w_akirako',
+      },
+      {
+        name: 'email',
+        href: 'mailto:w.akirako@gmail.com',
+      },
+    ],
+  },
+  seo: { twitter: '@w_akirako' },
 }
--- src/content/spec/about.md
+++ src/content/spec/about.md
@@ -1,6 +1,10 @@
 ---
-title: 关于
-pubDate: 2024-12-06
+title: Akirako の BLOG
+pubDate: 2025-01-01
 ---
 
-此主题系 Hexo 主题 <a href="https://github.com/sumimakito/hexo-theme-typography">活版印字</a> 在 Astro 平台上的移植版本
+- 💻 一个菜菜的前端程序员，日常随意更新
+- ❤️ 趣味：映画鑑賞、日本語勉強する、喜欢的音乐风格是“City Pop”，也听华语歌、粤语歌和日语歌曲
+- 🎦 松下相机用户，也在努力学习制作赏心悦目的影像
+- 📧 Reach me by [w.akirako@gmail.com](mailto:w.akirako@gmail.com)
+- 🌟 Nice to meet you
